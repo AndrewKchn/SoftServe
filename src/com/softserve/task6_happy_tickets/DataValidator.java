751739cad1b74d60fd255a0f3f7c4570d796f394
AndrewKchn <andrewkchn@gmail.com>
2020-02-25 08:32:33 +0200

    Display is util

--- src/com/softserve/task6_happy_tickets/DataValidator.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.task6_happy_tickets;
-
-public class DataValidator {
-
-    static boolean isCorrectForTicket(String s){
-        return s.matches("\\d{6}");
-    }
-
-}
--- src/com/softserve/util/Display.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.util;
-
-public class Display {
-
-    public static void show(String output){
-        System.out.println(output);
-    }
-
-}
